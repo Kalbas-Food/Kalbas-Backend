e6a563c965f752c8e37297ef23ed1068e8939e2f
Mahdi Haeri <mahdihaerim@gmail.com>
2025-04-17 20:30:15 +0330

    Add env variables to project

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,8 +19,11 @@ repositories {
 dependencies {
 //    implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.hibernate.orm:hibernate-envers:6.2.6.Final") // Add Hibernate Envers
+    implementation("jakarta.persistence:jakarta.persistence-api:3.1.0") // Add Jakarta Persistence API
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     runtimeOnly("org.postgresql:postgresql")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
@@ -38,3 +41,4 @@ kotlin {
 tasks.withType<Test> {
     useJUnitPlatform()
 }
+
