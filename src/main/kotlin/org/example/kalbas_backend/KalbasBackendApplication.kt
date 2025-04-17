325622739e64ae4da583954abd8d4749fefe5409
Mahdi Haeri <mahdihaerim@gmail.com>
2025-04-17 13:25:00 +0330

    Initial commit

---
+++ src/main/kotlin/org/example/kalbas_backend/KalbasBackendApplication.kt
@@ -0,0 +1,11 @@
+package org.example.kalbas_backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KalbasBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<KalbasBackendApplication>(*args)
+}
